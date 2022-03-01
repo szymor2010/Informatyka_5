75b55cfaad01c29178cf1fbe80cf2dd9e93d5973
szymor2010 <szymon.w.chmurze@gmail.com>
2022-03-01 11:18:43 +0100

    komentarze

--- Srednia/Srednia.py
+++ Srednia/Srednia.py
@@ -1,10 +1,12 @@
 import os
 
+#wczytanie pliku z ocenami
 with open("oceny.txt") as plik:
     oceny_str = plik.readline()
 
 oceny = list(map(int, oceny_str.split(";")))
 
+#wczytanie pliku z wagami
 with open("wagi.txt") as plik:
     wagi_str = plik.readline()
 
@@ -13,6 +15,7 @@ wagi = list(map(int,wagi_str.split(";")))
 suma = 0
 suma_wag = 0
 
+#obliczenia średniej ważonej
 for o,w in zip(oceny, wagi):
     suma += o*w
     suma_wag += w
